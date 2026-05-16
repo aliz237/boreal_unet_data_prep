454e005d2f01fdf88dcd63a5ab7ed8732af30d5f
aliz237 <ali.zaringhalam@gmail.com>
2026-05-16 09:27:19 -0700

    env update with R interpreter and updated runners

--- run-data-prep.sh
+++ run-data-prep.sh
@@ -2,13 +2,24 @@
 set -euo pipefail
 basedir=$(dirname "$(readlink -f "$0")")
 mkdir -p output
+
+ATL08AGB="output/ATL08_AGB_${1}_${2}.parquet"
+
+# transform ICESat2 atl08 RH metrics to AGB density
+# using packaged OLS models (-b) fitted on plot level data
+conda run --live-stream --name data_prep Rscript ${basedir}/atl08_to_agb.R \
+    -a ${5} \
+    -b ${9} \
+    -o ${ATL08AGB}
+
+# Then use ICESat2 AGB (and RH metrics) as target variable in tfrecords
 conda run --live-stream --name data_prep python ${basedir}/data_prep.py \
       --tile_num ${1} \
       --year ${2} \
       --hls_path ${3} \
       --topo_path ${4} \
-      --atl08_path ${5} \
+      --atl08_path ${ATL08AGB} \
       --patch_size ${6} \
       --overlap ${7} \
       --rh ${8} \
-      --fire_path "${9:-}"
+      --fire_path "${10:-}"
