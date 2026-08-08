20ab6ffa04e85a1f1fd9c43c46c2ec6a5cae326a
Ali <ali.zaringhalam@gmail.com>
2026-08-08 19:35:35 +0000

    adjusting tfrecord extraction code to implement AGB growth rate constraint

--- data_prep.py
+++ data_prep.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 
 import numpy as np
+import pandas as pd
 import geopandas as gpd
 from osgeo import gdal
 import rasterio
@@ -57,15 +58,15 @@ def _bytes_feature(value):
 def serialize_image_patch(hls_atl08_arr, patch_size, num_bands):
     arr_ser = tf.io.serialize_tensor(hls_atl08_arr)
     feature = {
-        'height': _int64_feature(patch_size),
-        'width': _int64_feature(patch_size),
-        'depth': _int64_feature(num_bands),
+        # 'height': _int64_feature(patch_size),
+        # 'width': _int64_feature(patch_size),
+        # 'depth': _int64_feature(num_bands),
         'arr': _bytes_feature(arr_ser)
     }
     ex = tf.train.Example(features=tf.train.Features(feature=feature))
     return ex.SerializeToString()
 
-def is_lidar_heavy(atl08_arr, patch_size, min_valid_lidar_per_batch, nodata=-9999):
+def is_lidar_heavy(atl08_arr, min_valid_lidar_per_batch, nodata=-9999):
     # filtering out the low quality atl08 patches
     return np.sum(atl08_arr != nodata) > min_valid_lidar_per_batch
 
@@ -125,98 +126,109 @@ def normalize_bands(in_raster_path, out_raster_path, band_defs, band_names, mask
     return out_raster_path
 
 def extract_patches_tfrec(
-    hls_path,
-    atl08_path,
-    topo_path,
-    tfrecord_path,
+    hls_paths, # dict of {year: path} to all hls years
+    atl08_paths, # dict of {year: path} to all atl08 years
+    topo_path, # there's only one topo path
+    tfrecord_path, # output tfrecord path
     patch_size=128,
     ndval=-9999,
-    overlap=32
+    overlap=32,
+    ndval_thresh=0.30
 ):
-    hls = rasterio.open(hls_path)
-    h = hls.height
-    w = hls.width
-    c = hls.count
-
-    atl08 = rasterio.open(atl08_path)
-    topo = rasterio.open(topo_path)
-
-    assert overlap >= 0 and overlap < patch_size, "invalid overlap!"
-    assert patch_size >= 32, "small patch size!"
-
-    tfrecord_path = Path(tfrecord_path)
+    years = sorted(list(hls_paths.keys()))
     step_size = patch_size - overlap
     n = 0
-    hls_patches_dropped = 0
-    topo_patches_dropped = 0
-    ndval_thresh = 0.30
-    patch_depth = hls.count + topo.count + atl08.count
-    print(hls.count, topo.count, atl08.count, patch_depth)
-    # patch_depth = 11 # 6 HLS spectral channels, 1 NBR, 1 slope, 1 TSRI, and 2 atl08 label.
-    # 120 is median valid pixel count of lidar track in ATL08 128x128 patches
-    # the other one is 70% of diagonal of a patch (so close to complete and decent lidar track)
-    min_valid_lidar_per_batch = int(min(patch_size * np.sqrt(2) * 0.7, 120))
+    min_n = int(min(patch_size * np.sqrt(2) * 0.7, 120))
     tfw = tf.io.TFRecordWriter(
         str(tfrecord_path), options=tf.io.TFRecordOptions(compression_type="GZIP")
     )
+    all_dims = set()
+    for t1, t2 in zip(years[:-1], years[1:]):
+        logger.info(f't1:{t1}, t2:{t2}')
+        with (
+            rasterio.open(hls_paths[t1]) as h1,
+            rasterio.open(hls_paths[t2]) as h2,
+            rasterio.open(atl08_paths[t1]) as a1,
+            rasterio.open(atl08_paths[t2]) as a2,
+            rasterio.open(topo_path) as tp
+        ):
+            patch_depth = h1.count + tp.count + a1.count
+            # patch_depth = 11 # 6 HLS spectral channels, 1 NBR, 1 slope, 1 TSRI, and 2 atl08 label.
+            # 120 is median valid pixel count of lidar track in ATL08 128x128 patches
+            # the other one is 70% of diagonal of a patch (so close to complete and decent lidar track)
+            for j in range(0, h1.width - patch_size, step_size):
+                for i in range(0, h1.height - patch_size, step_size):
+                    # (j, i) is the top-left corner of patch
+                    win = Window(j, i, patch_size, patch_size)
 
-    for j in range(0, w - patch_size, step_size):
-        for i in range(0, h - patch_size, step_size):
-            # (j, i) is the top-left corner of patch
-            # read ATL08 patch (1-band), if all null continue
-            win = Window(j, i, patch_size, patch_size)
-            lab_arr = atl08.read(window=win).astype(np.float32)
-            if lab_arr.ndim != 3: # could be just RH98 lyr or include AGB lyr
-                lab_arr = lab_arr[np.newaxis, ...]
-            # look for a close to diagonal lidar track across the patch
-            if not is_lidar_heavy(lab_arr[0], patch_size, min_valid_lidar_per_batch):
-                logger.debug("sparse lidar covergae, dropping patch")
-                continue
-            # read corresponding HLS patch
-            hls_arr = hls.read(window=win).astype(np.float32)
-            # can't have nulls in HLS, if >= ndval_thresh % is null for any band, continue
-            hls_arr[hls_arr == ndval] = np.nan
-            if np.any(
-                np.isnan(hls_arr).sum(axis=(1, 2)) >= ndval_thresh * patch_size**2
-            ):
-                logger.info("sparse HLS covergae, dropping patch")
-                continue
-            # fill NA
-            if not gapfill(hls_arr):
-                logger.info("Not gapfilling HLS, dropping patch")
-                hls_patches_dropped += 1
-                continue
-            # same for topo
-            topo_arr = topo.read(window=win).astype(np.float32)
-            topo_arr[topo_arr == ndval] = np.nan
-            if np.any(
-                np.isnan(topo_arr).sum(axis=(1, 2)) >= ndval_thresh * patch_size**2
-            ):
-                logger.info("sparse TOPO covergae, dropping patch")
-                continue
-            # fill NA
-            if not gapfill(topo_arr):
-                topo_patches_dropped += 1
-                continue
-            # save patches on disk
-            n += 1
-            # prep to write as TFrecord
-            # concat hls, topo features and atl08 label to build one training example
-            arr = np.concatenate([hls_arr, topo_arr, lab_arr])
-            # reorder as needed by model.fit, channels last
-            arr = np.moveaxis(arr, 0, -1)
-            # serialize the arr to write as a tfrecord
-            ser = serialize_image_patch(arr, patch_size, patch_depth)
-            tfw.write(ser.numpy())
-            if n % 100 == 0:
-                logger.info(f"wrote {n} records, of total {int(h*w/patch_size**2)}")
-
+                    # read ATL08 patch (1-band)
+                    a1_arr = a1.read(window=win).astype(np.float32)
+                    a2_arr = a2.read(window=win).astype(np.float32)
+                    if a1_arr.ndim != 3: # could be just RH98 lyr or include AGB lyr
+                        a1_arr = a1_arr[np.newaxis, ...]
+                        a2_arr = a2_arr[np.newaxis, ...]
+                    # look for an ~diagonal lidar track across the patch
+                    lh1 = is_lidar_heavy(a1_arr[0], min_n)
+                    lh2 = is_lidar_heavy(a2_arr[0], min_n)
+                    if not (is_lidar_heavy(a1_arr[0], min_n) or is_lidar_heavy(a2_arr[0], min_n)):
+                        logger.debug("sparse lidar covergae, dropping patch")
+                        continue
+                    logger.info(f'{t1}:{lh1}, {t2}:{lh2}')
+                    # read corresponding HLS patch
+                    h1_arr = h1.read(window=win).astype(np.float32)
+                    h2_arr = h2.read(window=win).astype(np.float32)
+                    # can't have nulls in HLS, if >= ndval_thresh % is null for any band, continue
+                    h1_arr[h1_arr == ndval] = np.nan
+                    h2_arr[h2_arr == ndval] = np.nan
+                    if np.any(
+                        np.isnan(h1_arr).sum(axis=(1, 2)) >= ndval_thresh * patch_size**2
+                    ) or np.any(
+                        np.isnan(h2_arr).sum(axis=(1, 2)) >= ndval_thresh * patch_size**2
+                    ):
+                        logger.info("sparse HLS covergae, dropping patch")
+                        continue
+                    # fill NA
+                    if not (gapfill(h1_arr) and gapfill(h2_arr)):
+                        logger.info("Not gapfilling HLS, dropping patch")
+                        continue
+                    # same for topo
+                    tp_arr = tp.read(window=win).astype(np.float32)
+                    tp_arr[tp_arr == ndval] = np.nan
+                    if np.any(
+                        np.isnan(tp_arr).sum(axis=(1, 2)) >= ndval_thresh * patch_size**2
+                    ):
+                        logger.info("sparse TOPO covergae, dropping patch")
+                        continue
+                    # fill NA
+                    if not gapfill(tp_arr):
+                        continue
+                    # save patches on disk
+                    n += 1
+                    # prep to write as TFrecord
+                    # concat hls, topo features and atl08 label to build one training example
+                    arr1 = np.concatenate([h1_arr, tp_arr, a1_arr])
+                    arr2 = np.concatenate([h2_arr, tp_arr, a2_arr])
+                    # reorder as needed by model.fit, channels last
+                    arr1 = np.moveaxis(arr1, 0, -1)
+                    arr2 = np.moveaxis(arr2, 0, -1)
+                    arr = np.stack([arr1, arr2], axis=0)
+                    all_dims.add((h1_arr.shape, h2_arr.shape,
+                                 tp_arr.shape,
+                                 a1_arr.shape, a2_arr.shape,
+                                 arr1.shape, arr2.shape, arr.shape))
+                    # serialize the arr to write as a tfrecord
+                    ser = serialize_image_patch(arr, patch_size, patch_depth)
+                    tfw.write(ser.numpy())
+                    if n % 100 == 0:
+                        logger.info(f"wrote {n} records")
     tfw.close()
-    hls.close()
-    atl08.close()
-    topo.close()
+    if len(all_dims) == 1:
+        logger.info(f'Shapes: {all_dims}')
+    else:
+        logger.info(f'shape mismatch ...')
+
     if n == 0:
-        print(f"No patches extracted from {hls_path}!")
+        logger.info(f"No patches extracted from {hls_paths[t1]}!")
         tfrecord_path.unlink(missing_ok=True)
     else:
         # rename the tfrecord file to include the record count
@@ -225,19 +237,18 @@ def extract_patches_tfrec(
                 tfrecord_path.name.replace(".tfrecord", f"_{n}.tfrecord")
             )
         )
-        print(
-            f"""{n} records saved,
-              {hls_patches_dropped} hls_patches_dropped,
-              {topo_patches_dropped} topo_patches_dropped"""
-        )
+        logger.info(f'{n} records saved')
 
 
 def atl08_to_raster(atl08_path, hls_path, out_raster_path,
                     ndval=-9999, rh='h_canopy', agb=False):
+    logger.info(f'out_raster_path:{out_raster_path}')
+    logger.info(f'hls_path: {hls_path}')
     df = gpd.read_parquet(atl08_path)
     with rasterio.open(hls_path) as hls:
         meta = hls.meta.copy()
         h, w = hls.height, hls.width
+        logger.info(f'h:{h}, w:{w}')
         rows, cols = rasterio.transform.rowcol(
             hls.transform,
             df.geometry.x.values,
@@ -252,12 +263,13 @@ def atl08_to_raster(atl08_path, hls_path, out_raster_path,
     if agb:
         out[1, rows, cols] = df['AGB'].values[mask] / Consts.MAX_AGB
     meta.update({"count": nlyrs, "nodata": ndval, "dtype": "float32"})
-
+    logger.info(out[out!=ndval][:5])
     with rasterio.open(out_raster_path, "w", **meta) as o:
         o.write(out)
         o.descriptions = (rh, 'AGB') if agb else (rh,)
         o.scales = (Consts.MAX_HEIGHT, Consts.MAX_AGB) if agb else (Consts.MAX_HEIGHT,)
         o.offsets = (0.0, 0.0) if agb else (0.0, )
+        logger.info(f'wrote {out_raster_path}')
 
 
 def get_extent(ds):
@@ -357,69 +369,85 @@ def create_fire_mask(fire_path, hls_path, year):
     return out_path
 
 def create_training_dataset(
-    tile_num, year, atl08_path, hls_path, topo_path, patch_size=128,
-    overlap=32, rh='h_canopy', fire_path='', agb=True, out_dir='output'
+    tile_num, atl08_tindex, hls_tindex, topo_tindex, patch_size=128,
+    overlap=32, rh='h_canopy', fire_path='', agb=True, out_dir='output', ndval_thresh=0.30
 ):
-
-    if fire_path:
-        mask_path = create_fire_mask(fire_path, hls_path, year)
-        if mask_path is None:
-            print(f'no fires intersect atl08-{year}, no tfrecords will be created.')
-            return None
-    else:
-        mask_path = None
-
-    print("rasterizing atl08 to HLS grid")
-    atl08_raster_path = str(Path(atl08_path).with_suffix(".tif"))
-    try:
-        atl08_to_raster(atl08_path, hls_path, atl08_raster_path, rh=rh, agb=agb)
-        hls_path = normalize_bands(
-            hls_path,
-            hls_path.replace('.tif', '_norm.tif'),
+    # if fire_path:
+    #     mask_path = create_fire_mask(fire_path, hls_path, year)
+    #     if mask_path is None:
+    #         print(f'no fires intersect atl08-{year}, no tfrecords will be created.')
+    #         return None
+    # else:
+    #     mask_path = None
+    a = pd.read_csv(atl08_tindex)
+    h = pd.read_csv(hls_tindex)
+    t = pd.read_csv(topo_tindex)
+    atl08_paths = a[a.tile_num==tile_num][['year', 's3_path']].set_index('year').to_dict()['s3_path']
+    hls_paths = h[h.tile_num==tile_num][['year', 's3_path']].set_index('year').to_dict()['s3_path']
+    topo_path = t.set_index('tile_num').loc[tile_num, 's3_path']
+    logger.info(f'atl08_paths:{atl08_paths}, hls_paths:{hls_paths}, topo_path:{topo_path}')
+    hls_norm_paths = dict()
+    atl08_raster_paths = dict()
+    logger.info("Normalizing topo")
+    topo_norm_path = normalize_bands(
+        topo_path,
+        str(Path('/tmp')/(Path(topo_path).stem + '_norm.tif')),
+        Consts.TOPO_BANDS,
+        ['slope', 'tsri']
+    )
+    for year in atl08_paths.keys():
+        atl08_raster_paths[year] = str(Path('/tmp')/(Path(atl08_paths[year]).stem + '_norm.tif'))
+        logger.info(f"rasterizing atl08 {year} to HLS grid")
+        atl08_to_raster(
+            atl08_paths[year],
+            hls_paths[year],
+            atl08_raster_paths[year],
+            rh=rh,
+            agb=agb
+        )
+        logger.info(f"Normalizing HLS {year}")
+        hls_norm_paths[year] = normalize_bands(
+            hls_paths[year],
+            str(Path('/tmp')/(Path(hls_paths[year]).stem + '_norm.tif')),
             Consts.HLS_BANDS,
             ['blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'nbr']
         )
-        topo_path = normalize_bands(
-            topo_path,
-            topo_path.replace('.tif', '_norm.tif'),
-            Consts.TOPO_BANDS,
-            ['slope', 'tsri']
-        )
 
-        print(f"Extracting patches for tile-year: {tile_num}-{year}")
-        extract_patches_tfrec(
-            hls_path,
-            atl08_raster_path,
-            topo_path,
-            tfrecord_path=f"{out_dir}/{tile_num}_{year}_{patch_size}_{overlap}.tfrecord.gz",
-            patch_size=patch_size,
-            overlap=overlap
-        )
-    except Exception as e:
-        print(e)
-    finally:
-        Path(topo_path).unlink(missing_ok=True)
-        Path(hls_path).unlink(missing_ok=True)
-        Path(atl08_raster_path).unlink(missing_ok=True)
+
+    logger.info(f"Extracting patches for tile: {tile_num}")
+    extract_patches_tfrec(
+        hls_norm_paths,
+        atl08_raster_paths,
+        topo_norm_path,
+        tfrecord_path=Path(f"{out_dir}/{tile_num}_2019_2024_{patch_size}_{overlap}.tfrecord.gz"),
+        patch_size=patch_size,
+        overlap=overlap,
+        ndval_thresh=ndval_thresh
+    )
+    # Path(topo_norm_path).unlink(missing_ok=True)
+    # for year in atl08_paths.keys():
+    #     Path(hls_norm_paths.get(year)).unlink(missing_ok=True)
+    #     Path(atl08_raster_paths.get(year)).unlink(missing_ok=True)
 
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser(
-        description="Creates a tfrecord.gz from patches of HLS, TOPO, and ATL08 for a given tile-year"
+        description="Creates a tfrecord.gz from patches of HLS, TOPO, and ATL08 for tile_num"
     )
-    parse.add_argument("--tile_num", help="boreal tile number", required=True)
-    parse.add_argument("--year", help="atl08 year", required=True)
-    parse.add_argument("--hls_path", help="HLS image path", required=True)
+    parse.add_argument("--tile_num", help="boreal tile number", type=int, required=True)
+    parse.add_argument("--hls_tindex", help="HLS tindex path", required=True)
     parse.add_argument("--fire_path", help="fire polygon path", required=False, default='')
     parse.add_argument(
-        "--topo_path", help="topo image path with slope as second band", required=True
+        "--topo_tindex", help="topo tindex path", required=False,
+        default='s3://maap-ops-workspace/shared/montesano/DPS_tile_lists/run_build_stack_topo/build_stack_v2024_2/CopernicusGLO30/Topo_tindex_master.csv'
     )
-    parse.add_argument("--atl08_path", help="atl08 parquet file", required=True)
+    parse.add_argument("--atl08_tindex", help="atl08 tindex path", required=True)
     parse.add_argument("--patch_size", help="training image patch size", type=int, default=128)
     parse.add_argument("--overlap", help="overlap between training patches", type=int, default=32)
     parse.add_argument("--rh", help="RH metric to include in tfrecords", default='h_canopy')
     parse.add_argument("--out_dir", help="output directory name", default='output')
-    parse.add_argument("--agb", help="Pass to include AGB layer in output tfrecords", action='store_true', default=True)
+    parse.add_argument("--agb", help="Pass to include AGB layer in output tfrecords", action='store_true', default=False)
+    parse.add_argument("--ndval_thresh", help="Drop the training patch if HLS nodata %% > ndval_thresh", type=float, default=0.30)
 
     args = parse.parse_args()
 
