ca682bb0c568eb66dced12e098fe90bcde1b3d46
Ali <ali.zaringhalam@gmail.com>
2026-08-15 02:43:51 +0000

    remove stale files

--- build-env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S bash --login
-set -euo pipefail
-# This script is used to install any custom packages required by the algorithm.
-
-# Get current location of build script
-basedir=$( cd "$(dirname "$0")" ; pwd -P )
-
-conda env create -f ${basedir}/environment.yml
